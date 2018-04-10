0a6653d2f64bd4f68ac506816368c42d83812346
Conner Gillette <connerwgillette@gmail.com>
2018-04-09 22:47:38 -0700

    Add changes up to Lecture 61

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,3 +1,11 @@
-export default function(state = null, action) {
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state = [], action) {
+    switch (action.type) {
+        case FETCH_WEATHER:
+            // return state.push(action.payload.data); // BAD!
+            // return state.concat([action.payload.data]); // BETTER!
+            return [action.payload.data, ...state]; // BEST! (ES6)
+    }
     return state;
 }
